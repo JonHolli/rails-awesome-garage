eeaed43113e2cda8ede0a7b0b13b1a004fe60993
Jon Hollingsworth <jondriveuk@gmail.com>
2024-10-03 17:45:19 +0100

    2

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
+gem "simple_form"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.2.1"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
@@ -52,4 +53,3 @@ group :development do
   # Highlight the fine-grained location where an error occurred [https://github.com/ruby/error_highlight]
   gem "error_highlight", ">= 0.4.0", platforms: [:ruby]
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning the DB..."
+Car.destroy_all
+Owner.destroy_all
+
+puts "Creating owners..."
+owner1 = Owner.create(nickname: "Bob")
+owner2 = Owner.create(nickname: "Jim")
+owner3 = Owner.create(nickname: "Margaret")
+owner4 = Owner.create(nickname: "Louie")
+
+puts "Creating cars..."
+Car.create!(brand: "Rolls Royce", model: "Phantom", fuel: "Unleaded Petrol", year: 1962, owner: owner1)
+Car.create!(brand: "Ford", model: "Cortina", fuel: "Diesel", year: 1972, owner: owner2)
+Car.create!(brand: "Vauxhall", model: "Cavalier", fuel: "Unleaded Petrol", year: 1981, owner: owner3)
+Car.create!(brand: "BMW", model: "5 Series", fuel: "Unleaded Petrol", year: 1994, owner: owner4)
+
+puts "Done! #{Owner.count} owners and #{Car.count} cars created"
